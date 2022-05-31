7aad1ee65c711128e2168de555b14d6a136457d4
이석호 <mercen@mercen.net>
2022-05-31 17:40:56 +0900

    Meal Function Updated

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#define MEALURL "https://open.neis.go.kr/hub/mealServiceDietInfo?KEY=85d56b3110fa4a2bbb700a1106bb58bf&Type=json&ATPT_OFCDC_SC_CODE=D10&SD_SCHUL_CODE=7240454&MLSV_YMD="
+
 // //
 // //  main.cpp
 // //  도발도발 프로젝트
@@ -18,6 +20,7 @@
 #include <iostream>    // C++ 표준 입출력 iostream 라이브러리
 #include <ctime>       // 현재 날짜 호출을 위한 time 라이브러리
 #include <string>      // 문자열 제어를 위한 string 라이브러리
+#include <regex>       // 문자열 치환을 위한 정규식 regex 라이브러리
 
 using namespace std;   // std 네임스페이스 사용
 string name, token;
@@ -27,7 +30,7 @@ string name, token;
 
 void Clear() {
 
-    cout << "\x1B[2J\x1B[H" << endl; // 화면에 있는 모든 로그 삭제
+    cout << "\x1B[2J\x1B[H\e[?25l" << endl; // 화면에 있는 모든 로그 삭제
 
     cout << "=============================== [ 도발도발 0.1 ] ===============================" << endl;
     cout << "--------------------------------------------------------------------------------" << endl;
@@ -55,6 +58,26 @@ size_t callback(void *ptr, size_t size, size_t nmemb, string* data) {
 
 	data->append((char*)ptr, size * nmemb);
     return size * nmemb;
+
+}
+
+
+// HTTP GET 통신 함수
+
+string getdata(string url) {
+
+    string response; CURL *curl = curl_easy_init(); // libcurl 세팅
+
+    struct curl_slist *list = NULL; // 헤더 리스트 선언
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str()); // URL 설정
+
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response); // 응답 송신
+
+    curl_easy_perform(curl); curl_slist_free_all(list);
+    curl_easy_cleanup(curl); curl_global_cleanup(); // libcurl 정리
+
+    return response;
 
 }
 
@@ -99,7 +122,7 @@ void login() {
         else if (key == 127 && id.length()) { // 백스페이스 구현
             id = id.substr(0, id.length() - 1);
         }
-        Clear(); cout << "\n# 도담도담 로그인\n" << endl;
+        Clear(); cout << "\n\e[?25h# 도담도담 로그인\n" << endl;
         cout << "  * 아이디: " << id; // 화면 업데이트
         key = getch(); // 키 값 받아오기
     }
@@ -115,7 +138,7 @@ void login() {
             pwi = pwi.substr(0, pwi.length() - 1);
             pwj = pwj.substr(0, pwj.length() - 1);
         }
-        Clear(); cout << "\n# 도담도담 로그인\n" << endl;
+        Clear(); cout << "\n\e[?25h# 도담도담 로그인\n" << endl;
         cout << "  * 아이디: " << id << endl;
         cout << "  * 비밀번호: " << pwj; // 화면 업데이트
         key = getch(); // 키 값 받아오기
@@ -148,15 +171,29 @@ void login() {
 
 void meal() {
 
-    cout << "\n# 식단표 확인\n" << endl;
+    string meals, parsed, response; int command;
 
+    cout << "\n# 식단표 확인\n" << endl;
+    string title[3] = {"조식", "중식", "석식"};
+    for(int i=0; i<3; i++) cout << "  - " << i+1 << ". " << title[i] << endl;
+
+    command = getch()-48; Clear();
+
+    cout << "\n# 오늘의 " << title[command-1] << "\n\n  - ";
     time_t now = time(0); struct tm tstruct;
 
     char date[80]; tstruct = *localtime(&now);
     strftime(date, sizeof(date), "%Y%m%d", &tstruct);
 
     string todate(date);
-    cout << todate;
+    response = getdata(MEALURL + todate + "&MMEAL_SC_CODE=" + to_string(command));
+    Json::Reader reader; Json::Value root; // JSON 변수 미리 선언  
+    reader.parse(response, root); // JSON 파싱 준비
+
+    parsed = root["mealServiceDietInfo"][1]["row"][0]["DDISH_NM"].asString();
+    meals = regex_replace(parsed, regex("<br/>"), "\n  - ");
+
+    cout << meals << endl; if (getch() == 10) return;
 
 }
 
@@ -166,16 +203,17 @@ void meal() {
 int main() {
     
     Clear(); // 화면 지우기
-    cout << "\n# 도담도담 로그인\n" << endl; login(); // 도담도담 로그인
+    cout << "\n\e[?25h# 도담도담 로그인\n" << endl; login(); // 도담도담 로그인
 
     while(1) { int command;
         Clear(); cout << "\n# 환영합니다, " << name << "님!\n" << endl; // 환영 메시지 출력
         string title[4] = {"식단표 확인", "기상송 확인", "자습실 신청", "외출·외박 신청"};
+
         for(int i=0; i<4; i++) cout << "  - " << i+1 << ". " << title[i] << endl;
-        cout << "\n  * 입력: "; cin >> command; Clear(); // 명령어 입력
+        command = getch()-48; Clear(); // 명령어 입력
 
         if (command==1) meal();
-        else return 0;
+        else { cout << "\e[?25h"; return 0; }
     }
     
 }
